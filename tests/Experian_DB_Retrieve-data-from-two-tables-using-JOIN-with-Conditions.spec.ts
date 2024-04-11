4a30c7bf2edef5c983aa8c10827de0d8f0e61d7d
Abraham <166658472+acastanedagonzalez@users.noreply.github.com>
2024-04-11 16:01:41 -0600

    Update Experian_DB_Retrieve-data-from-two-tables-using-JOIN-with-Conditions.spec.ts

--- tests/Experian_DB_Retrieve-data-from-two-tables-using-JOIN-with-Conditions.spec.ts
+++ tests/Experian_DB_Retrieve-data-from-two-tables-using-JOIN-with-Conditions.spec.ts
@@ -20,9 +20,7 @@ async function establishDBConnection() {
     await loginPage.loginWith("testUser", "Test123")
     //Execute Query to tables
     //Join id from table1.id and table2.id.
-    await connectToDB.executeQuery('USE [db Name] (NOLOCK) SELECT * FROM table1 INNER JOIN table2 ON table1.id = table2.id; ')
     //Include only records >=40 from table2.age.
-    await connectToDB.executeQuery('USE [db Name] (NOLOCK) SELECT * FROM table2 WHERE table2.age >=40; ')
     //Sort ASC from table1.name
-    await connectToDB.executeQuery('select * from table1 ORDER BY table1.name ASC; ')
+    await connectToDB.executeQuery('USE [db Name] (NOLOCK) SELECT * FROM table1 INNER JOIN table2 ON table1.id = table2.id AND SELECT * FROM table2 WHERE table2.age >=40 AND ORDER BY table1.name ASC; ')
   });
